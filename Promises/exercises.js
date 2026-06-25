50e405a832627ec3e6247c664b12aa7c9f8535da
Justin <apolongjustin49@gmail.com>
2026-06-25 08:17:58 +0800

    answered exercise 3 for promise

--- Promises/exercises.js
+++ Promises/exercises.js
@@ -6,7 +6,19 @@ const promise = new Promise((resolve) => {
 });
 */
 
+/*
 const promise = Promise.reject(setTimeout(() => {
     console.log('Something went wrong');
 }, 1000));
 promise.catch(console.error);
+*/
+
+/*
+function getNumber() {
+    return Promise.resolve(setTimeout(() => {
+        console.log(100);
+    }, 3000));
+};
+getNumber().then(console.log);
+*/
+
