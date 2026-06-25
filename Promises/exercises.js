e0e319a2915952f12ae423a0df1fa0374de2d46a
Justin <apolongjustin49@gmail.com>
2026-06-25 08:09:24 +0800

    answered the second exercise for learning promise

--- Promises/exercises.js
+++ Promises/exercises.js
@@ -1,5 +1,12 @@
+/*
 const promise = new Promise((resolve) => {
     setTimeout(() => {
         console.log('Hello Promise');
     }, 3000);
 });
+*/
+
+const promise = Promise.reject(setTimeout(() => {
+    console.log('Something went wrong');
+}, 1000));
+promise.catch(console.error);
