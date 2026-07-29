cfcbf469e640a61302bb8e51847712f5325c93f1
Justin <apolongjustin49@gmail.com>
2026-07-29 09:11:17 +0800

    added a file for handling different HTTP methods

---
+++ HTTP/7th.js
@@ -0,0 +1,21 @@
+// Handling different HTTP methods
+if (req.method === "GET") {
+
+}
+
+if (req.method === "POST") {
+
+}
+
+if (req.method === "PUT") {
+
+}
+
+if (req.method === "DELETE") {
+
+}
+
+//Professional servers always check both URL and method.
+if (req.url === "/users" && req.method === "GET") {
+
+}
