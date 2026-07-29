d8a748c9af0c4bf74308b1d1b2b11daa648a3e4a
Justin <apolongjustin49@gmail.com>
2026-07-29 09:30:16 +0800

    added a file for status codes in node.js

---
+++ HTTP/12th.js
@@ -0,0 +1,19 @@
+// Status Codes
+
+200 // OK
+
+201 // Created
+
+204 // No Content
+
+301 // Moved Permanently
+
+400 // Bad Request
+
+401 // Unauthorized
+
+403 // Forbidden
+
+404 // Not Found
+
+500 // Internal Server Error
