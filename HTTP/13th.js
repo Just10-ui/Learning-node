dafc2e9653acbbea3777ad03430c3143b6082c03
Justin <apolongjustin49@gmail.com>
2026-07-29 13:02:43 +0800

    added a file for serving different content types in node.js

---
+++ HTTP/13th.js
@@ -0,0 +1,22 @@
+// Serving Different COntent types
+
+//HTML
+"text/html"
+
+//CSS
+"text/css"
+
+//JS
+"text/javascript"
+
+//JSON
+"application/json"
+
+//PNG
+"image/png"
+
+//JPEG
+"image/jpeg"
+
+//PDF
+"application/pdf"
