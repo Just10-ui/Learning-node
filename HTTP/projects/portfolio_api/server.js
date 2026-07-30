9df85665a5b8dadeff8f0f148018a41a7fa449d7
Justin <apolongjustin49@gmail.com>
2026-07-30 08:30:33 +0800

    added a project folder for mini HTTP projects

---
+++ HTTP/projects/portfolio_api/server.js
@@ -0,0 +1,47 @@
+const http = require('http');
+const portfolio = require('./data/user.json');
+const { success } = require('zod');
+const JSON_HEADER = {"Content-Type": "application/json"};
+const PORT = 8080;
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/' && req.method === 'GET') {
+
+        res.writeHead(200, JSON_HEADER);
+
+        return res.end(JSON.stringify({
+            success: true,
+            data: portfolio.data
+        }));
+
+    } else if (req.url === '/about' && req.method === 'GET') {
+
+        res.writeHead(200, JSON_HEADER);
+
+        return res.end(JSON.stringify({
+            success: true,
+            about: portfolio.about.about
+        }));
+
+    } else if (req.url === '/contact' && req.method === 'GET') {
+
+        res.writeHead(200, JSON_HEADER);
+
+        return res.end(JSON.stringify({
+            success: true,
+            contact: portfolio.contact
+        }));
+
+    } else {
+        res.writeHead(404, JSON_HEADER);
+
+        return res.end(JSON.stringify({
+            success: false,
+            message: "Router  not found"
+        }));
+    };
+});
+
+server.listen(PORT, () => {
+    console.log('Server is running at http://localhost:8080'); 
+});
