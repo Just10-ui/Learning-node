1d86cb5910e74f338caae4b3491ed7572d75766f
Justin <apolongjustin49@gmail.com>
2026-08-01 09:51:57 +0800

    created the server and GET method for students but still working on POST method

--- HTTP/projects/student_registration_api/server.js
+++ HTTP/projects/student_registration_api/server.js
@@ -1,2 +1,29 @@
 const http = require('http');
 const data = require('./data/student.json');
+const PORT = 8080;
+const JSON_HEADER = {
+    'Content-Type': 'application/json'
+};
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/students' && req.method === 'GET') {
+        res.writeHead(200, JSON_HEADER);
+
+        return res.end(JSON.parse({
+            'success': true,
+            'students': data.students
+        }));
+    } else if (req.url === '/students' && req.method === 'POST') {
+
+    } else {
+        res.writeHead(404, JSON_HEADER);
+
+        return res.end(JSON.parse({
+            'error': 'Route not found'
+        }));
+    }
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
