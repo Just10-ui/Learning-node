0b5c7780eec34ef2361437aaa0c616c9deafab75
Justin <apolongjustin49@gmail.com>
2026-08-15 13:56:49 +0800

    added a file for path.dirname method

---
+++ PATH/codes/6th.js
@@ -0,0 +1,6 @@
+//| path.dirname() - dirname() gets the directory portion of a path
+const filePath = '/users/Justin/documents/resume.pdf';
+
+console.log(path.dirname(filePath));
+
+// output - /users/Justin/documents
