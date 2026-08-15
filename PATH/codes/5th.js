6df5fc0e3d888052d948cc10623046b0d66a104f
Justin <apolongjustin49@gmail.com>
2026-08-15 13:56:12 +0800

    added a file for path.basename method

---
+++ PATH/codes/5th.js
@@ -0,0 +1,6 @@
+//| path.basename() - basename() gets the last part of path
+const filePath = '/users/Justin/documents/resume.pdf';
+
+console.log(path.basename(filePath));
+
+// output - resume.pdf 
